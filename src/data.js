1ac0cdfdf7ac4e944855a34398e5fc54fce9f890
Mahikagarg09 <mahika_garg@yahoo.com>
2022-11-17 11:18:11 +0530

    added the data of my portfolio

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    title: "TEXTUTILS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./textutils.png",
+    code:"https://github.com/Mahikagarg09/TextUtils.git",
+    link:"https://txtutils.netlify.app/",
+  },
+  {
+    title: "WEATHER-RADAR",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./weather-radar.gif",
+    link: "https://radarweather.netlify.app/:",
+    code:"https://github.com/Mahikagarg09/Weather-Search.git",
+  },
+  
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML5",
+  "CSS3",
+  "Bootstrap",
+  "JavaScript",
+  "React JS",
+  "JQuery",
+  "Node JS",
+  "Express",
+  "Rest API",
+  "Mongo DB",
+  "My SQL",
+  "Git",
+  "Github",
+  "ThunderClient",
+  "C",
+  "C++",
+  "JAVA",
+];
