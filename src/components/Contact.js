4a672a8e5663c02f98b05f93d397841b2bb95c37
Mahikagarg09 <mahika_garg@yahoo.com>
2022-11-17 11:23:47 +0530

    added emailjs to contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,75 @@
+import React,{useRef} from "react";
+import emailjs from 'emailjs-com';
+
+export default function Contact() {
+  const form=useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_pnhfbpi', 'template_4wykiug', form.current, '3RCmMfabPU1Rk-R3A')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <section id="contact" className="w-full h-screen p-4 text-white">
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full items-center">
+        <form
+          netlify
+          name="test"
+          className="flex flex-col w-full md:w-1/2"
+          ref={form}
+          onSubmit={sendEmail}>
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Contact Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Submit the form below to get in touch with me..
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
