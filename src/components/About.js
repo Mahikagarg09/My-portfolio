7d95f9ba65514f577d9edb740de1c9c888e09e27
Mahikagarg09 <mahika_garg@yahoo.com>
2022-11-18 21:12:57 +0530

    made more responsive

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ import SocialLinks from "./SocialLinks";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-5 py-20 md:flex-row flex-col items-center pl-24">
+      <div className="container mx-auto flex px-5 py-20 md:flex-row flex-col items-center pl-0 md:px-20">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font text-6xl sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Mahika Garg.
             <br className="hidden lg:inline-block" />I love to explore new things.
           </h1>
@@ -30,7 +30,7 @@ export default function About() {
           </div>
           <SocialLinks/>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 pl-24">
           <img
             className="object-cover object-center rounded-full w-3/4 h-4/5"
             alt="hero"
